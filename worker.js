58ab7f8532773afc3dc8e7b9c0ef1f71a636847e
Scott Olson <solson@zebra.com>
2022-08-04 12:23:30 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,31 @@
 export default {
-  fetch: req => new Response('Hello Scott')
+  fetch: (req, env) => {
+    // get durable object
+    const stub = env.COUNTER.get(env.COUNTER.idFromName(req.headers.get('cf-connecting-ip'))
+    
+    // fetch durable object
+    return stub.fetch(req)
+  }
 }
+
+export class Counter {
+  constructor(state, env) {
+    this.state = state;
+    // `blockConcurrencyWhile()` ensures no requests are delivered until
+    // initialization completes.
+    this.state.blockConcurrencyWhile(async () => {
+      let stored = await this.state.storage.get("value");
+      // After initialization, future reads do not need to access storage.
+      this.value = stored || 0;
+    });
+  }
+
+  // Handle HTTP requests from clients.
+  async fetch(request) {
+    // use this.value rather than storage
+    this.value = this.value++
+    this.state.storage.put("value", this.value)
+    return new Response(this.value)
+  }
+}
+
